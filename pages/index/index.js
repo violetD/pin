ced2762f9bfbbff02fe317b6869ae3f1b2c5af0c
violet deng <dengviolet@gmail.com>
2018-01-15 11:51:12 +0800

    拼红包

---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+//index.js
+//获取应用实例
+var app = getApp()
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    textRange: ['测试字符', '测试字符2'],
+    userInfo: {},
+    text: "",
+    time: "30秒",
+    tips: "0.00",
+    items: [{
+      name: '10秒',
+      value: 10
+    }, {
+      name: '20秒',
+      value: 20
+    }, {
+      name: '30秒',
+      value: 30,
+      checked: true
+    }]
+  },
+  //事件处理函数
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  setTextValue: function (e) {
+    this.setData({
+      text: this.data.textRange[e.detail.value]
+    })
+  },
+  selectTime: function () {
+    wx.showModal({
+      title: '设置挑战时间',
+      content: '',
+      showCancel: false,
+      success: function () {
+
+      }
+    })
+  },
+  showError: function (message) {
+    wx.showModal({
+      title: '提示',
+      content: message,
+      showCancel: false,
+      success: function () {
+
+      }
+    })
+  },
+  formSubmit: function (e) {
+    if (!e.detail.value.text) {
+      this.showError('请输入要拼接的文字内容');
+      return;
+    }
+    if (!e.detail.value.text.length > 24) {
+      this.showError('要拼接的文字内容不能超过24个字');
+      return;
+    }
+    if ('' === e.detail.value.money) {
+      this.showError('请输入总赏金');
+      return;
+    }
+    if ('' === e.detail.value.number) {
+      this.showError('请输入红包个数');
+      return;
+    }
+    console.log(e.detail.value)
+  }
+})
