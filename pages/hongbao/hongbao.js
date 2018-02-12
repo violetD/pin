5f4e64a461fdbe140001b986258b5dba26ed6d1a
violet deng <dengviolet@gmail.com>
2018-02-12 10:30:07 +0800

    将获取分页数据封装为class

--- pages/hongbao/hongbao.js
+++ pages/hongbao/hongbao.js
@@ -6,19 +6,21 @@ Page({
   data: {
     userInfo: {},
     currentTab: 0,
-    me: [],
     clientHeight: 400,
     swiperTabHeight: 100,
-    meScrollViewHeight: 200,
-    totalMoney: '0.00',
-    totalNumber: 0,
-    meLoadingText: '加载中。。。',
-    meLoadingShow: false,
-    totalRecieveMoney: '0.00',
-    totalRecieveNumber: 0,
-    recieve: [],
-    recieveLoadingText: '加载中。。。',
-    recieveLoadingShow: false,
+    scrollViewHeight: 200,
+    me: {
+      money: '0.00',
+      number: 0,
+      text: '加载中...',
+      list: []
+    },
+    recieve: {
+      money: '0.00',
+      number: 0,
+      text: '加载中...',
+      list: []
+    }
   },
   //事件处理函数
   onLoad () {
@@ -37,7 +39,7 @@ Page({
       .select('#meInfoView').boundingClientRect().exec((res) => {
       this.setData({
         swiperTabHeight: res[0].height,
-        meScrollViewHeight: res[1].height
+        scrollViewHeight: res[1].height
       })
     })
 
@@ -52,14 +54,102 @@ Page({
 
   onShow () {
 
-    app.request("/game/getAllSummary").then((data) => {
-      this.setData({
-        totalMoney: (data.data.money / 100).toFixed(2),
-        totalNumber: data.data.num,
-      })
-    }).catch(function () { })
+    this.me = new PageList({
+      sizeUrl: '/game/getAllSummary',
+      sizeCallback: (data) => {
+        this.setData({
+          ['me.money']: (data.data.money / 100).toFixed(2),
+          ['me.number']: data.data.num,
+        })
+      },
+      dataUrl: '/game/getAll',
+      dataCallback: (data) => {
+        this.setData({
+          ['me.list']: this.data.me.list.concat(data.list.map(function (item) {
+            return {
+              ...item,
+              formatCreateTime: util.formatTime(item.create_time)
+            }
+          }))
+        })
+      },
+      loading: (show) => {
+        if (show) {
+          this.setData({
+            ['me.text']: '加载中...'
+          })
+        } else {
+          this.setData({
+            ['me.text']: '下拉加载更多'
+          })
+        }
+      },
+      finished: () => {
+        if (this.data.me.number > 0) {
+          this.setData({
+            ['me.text']: '没有更多项目啦'
+          })
+        } else {
+          this.setData({
+            ['me.text']: '您还没有发过红包哦'
+          })
+        }
+      }
+    })
+    this.me.fetchSize()
 
-    this.loadList()
+    this.recieve = new PageList({
+      sizeUrl: '/game/getRecieveSummary',
+      sizeCallback: (data) => {
+        this.setData({
+          ['recieve.money']: (data.data.money / 100).toFixed(2),
+          ['recieve.number']: data.data.num,
+        })
+      },
+      dataUrl: '/game/getRecieveList',
+      dataCallback: (data) => {
+        this.setData({
+          ['recieve.list']: this.data.me.list.concat(data.list.map(function (item) {
+            return {
+              ...item,
+              formatMoney: (item.money / 100).toFixed(2),
+              formatCreateTime: util.formatTime(item.create_time)
+            }
+          }))
+        })
+      },
+      loading: (show) => {
+        if (show) {
+          this.setData({
+            ['recieve.text']: '加载中...'
+          })
+        } else {
+          this.setData({
+            ['recieve.text']: '下拉加载更多'
+          })
+        }
+      },
+      finished: () => {
+        if (this.data.recieve.number > 0) {
+          this.setData({
+            ['recieve.text']: '没有更多项目啦'
+          })
+        } else {
+          this.setData({
+            ['recieve.text']: '您还没有收到红包哦'
+          })
+        }
+      }
+    })
+  },
+  // TODO
+  scroll(e) {
+    console.log(e)
+    return
+    this.me.fetchData()
+  },
+  bindRecieveScroll: function () {
+    this.recieve.fetchData()
   },
 
   swichNav: function (e) {
@@ -72,88 +162,47 @@ Page({
     this.setData({
       currentTab: e.detail.current
     })
-    console.log(e.detail)
     if (e.detail.current == 1) {
       if (this.loaded) return
       this.loaded = true
-      app.request("/game/getRecieveSummary").then((data) => {
-        this.setData({
-          totalRecieveMoney: (data.data.money / 100).toFixed(2),
-          totalRecieveNumber: data.data.num,
-        })
-      }).catch(() => {})
-      this.loadRecieveList()
+      this.recieve.fetchSize()
     }
-  },
-  meStart: 0,
-  meListPageSize: 10,
-  meFinished: false,
-  bindMeScroll (e) {
-    if (this.meFinished) return
-    
-    this.loadList()
-  },
-  loadList () {
-    const that = this
-
-    this.setData({
-      meLoadingShow: true
-    })
-    
-    app.request("/game/getAll", {
-      start: this.meStart,
-      end: this.meListPageSize,
-    }).then((data) => {
-      if (data.list.length === 0 || data.list.length < this.meListPageSize) {
-        that.setData({
-          meLoadingText: '已加载完'
-        })
-        that.meFinished = true;
-      }
-      that.setData({
-        me: that.data.me.concat(data.list.map(function (item) {
-          return {
-            ...item,
-            formatCreateTime: util.formatTime(item.create_time)
-          }
-        }))
-      })
-      this.meStart += this.meListPageSize;
-    }).catch(function () {})
-  },
-  recieveStart: 0,
-  recieveListPageSize: 10,
-  recieveFinished: false,
-  bindRecieveScroll: function () {
-    if (this.recieveFinished) return;
-    this.loadRecieveList();
-  },
-  loadRecieveList: function () {
-    const that = this;
-
-    this.setData({
-      recieveLoadingShow: true
-    })
-    
-    app.request("/game/getRecieveList", {
-      start: this.recieveStart,
-      end: this.recieveListPageSize,
-    }).then(function (data) {
-      that.setData({
-        recieve: that.data.recieve.concat(data.list.map(function (item) {
-          return {
-            ...item,
-            formatCreateTime: util.formatTime(item.create_time)
-          }
-        }))
-      })
-      that.recieveStart += that.recieveListPageSize;
-      if (data.list.length === 0 || data.list.length < that.recieveListPageSize) {
-        that.setData({
-          recieveLoadingText: '已加载完'
-        })
-        that.recieveFinished = true;
-      }
-    }).catch(function () {})
   }
 })
+
+class PageList {
+
+  constructor (options) {
+    this.options = options
+    this.pageSize = options.pageSize || 10
+    this.start = 0
+    this.finished = false
+    this.loading = false
+  }
+
+  fetchSize () {
+    app.request(this.options.sizeUrl).then((data) => {
+      this.fetchData()
+      typeof this.options.sizeCallback === 'function' && this.options.sizeCallback(data)
+    }).catch(() => {})
+  }
+
+  fetchData () {
+    if (this.finished || this.loading) return
+    this.loading = true
+    typeof this.options.loading === 'function' && this.options.loading(true)
+    app.request(this.options.dataUrl, {
+      start: this.start,
+      end: this.pageSize,
+    }).then((data) => {
+      typeof this.options.dataCallback === 'function' && this.options.dataCallback(data)
+      this.start += this.pageSize
+      this.loading = false
+      typeof this.options.loading === 'function' && this.options.loading(false)
+      if (data.list.length === 0 || data.list.length < this.pageSize) {
+        typeof this.options.finished === 'function' && this.options.finished()
+        this.finished = true
+      }
+    }).catch(() => {})
+  }
+}
