7855ad258a255b8ffd834f462700d2aa7961ebaa
violet deng <dengviolet@gmail.com>
2018-02-12 10:50:37 +0800

    删除投诉页面不必要的回调

--- pages/appeal/appeal.js
+++ pages/appeal/appeal.js
@@ -17,66 +17,17 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad (options) {
     this.setData({ options, reasons })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
-
-  handleChange(e) {
+  handleChange (e) {
     this.setData({
       reason: e.detail.value
     })
   },
 
-  handleSubmit() {
+  handleSubmit () {
     if (!this.data.reason) {
       return wx.showModal({
         title: '提示',
